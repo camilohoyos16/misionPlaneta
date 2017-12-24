f1763981fb55ba3963d80db3a6206240ac8e64ed
camilohoyos16 <acamilohoyos@hotmail.com>
2017-12-23 22:37:48 -0500

    Start enerigia level

---
+++ Assets/Juegos/Energia/Scripts/Lights.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lights : MonoBehaviour {
+
+	[SerializeField] private float minTime;
+	[SerializeField] private float maxTime;
+	private float timeToTurnOn;
+
+
+	[Header("")]
+	[SerializeField] private Color offColor;
+	[SerializeField] private Color onColor;
+	private Button c_button;
+
+	private Image c_image;
+	private bool isOn;
+	// Use this for initialization
+	void Start () {
+		isOn = false;
+		c_button = GetComponent<Button> ();
+		c_button.interactable = false;
+		c_image = GetComponent<Image> ();
+		c_image.color = offColor;
+		StartTurnOnAgain ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator turnOnLight;
+	IEnumerator TurnOnLight()
+	{
+		yield return new WaitForSeconds (timeToTurnOn);
+		c_image.color = onColor;
+		isOn = true;
+		c_button.interactable = true;
+	}
+
+	private void StartTurnOnAgain()
+	{
+		timeToTurnOn = Random.Range (minTime, maxTime);
+		turnOnLight = TurnOnLight ();
+		StartCoroutine (turnOnLight);
+	}
+
+	public void OnLightClick()
+	{
+		c_image.color = offColor;
+		isOn = false;
+		c_button.interactable = false;
+		StartTurnOnAgain ();
+	}
+}
